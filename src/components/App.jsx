5b8856a3a65203e22e2d2fbf0adb9d36b2b47d1c
caleb <mcaleb808@gmail.com>
2019-03-28 14:55:29 +0200

    chore(Redux) setting up Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import React, { Component } from "react";
 import TestRedux from "./TestRedux/TestRedux";
+import "./App.scss";
 
 const App = () => (
   <div>
     <TestRedux />
   </div>
 );
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
-import App from "./components/App.jsx";
+import App from "./components/App";
 
+ReactDOM.render(<App />, document.getElementById("root"));
+
+module.hot.accept();
 render(
   <Provider store={store}>
     <App />
