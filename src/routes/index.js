851052cff0362346b13e5c5ed614a0e4fda7b43e
nkfab <fabjeezy@gmail.com>
2019-03-29 14:31:55 +0200

    chore(router): add router to the app [Finishes #163519094]

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import HomeComponent from "../views";
+import LoginComponent from "../views/Login";
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={HomeComponent} />
+          <Route exact path="/login" component={LoginComponent} />
+        </div>
+      </Router>
+    );
+  }
+}
+export default Routes;
---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const HomeComponent = () => {
+  return <h1 style={{ background: "gray", color: "white" }}>Home Component</h1>;
+};
+
+export default HomeComponent;
