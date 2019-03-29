2fd7c24574c0d2eddce17db9541430cea93d5d77
nkfab <fabjeezy@gmail.com>
2019-03-29 14:31:55 +0200

    chore(router): add router to the app [Finishes #163519094]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import "./App.scss";
+import Routes from "../routes";
 
-const App = () => (
-  <div>
-    <h1 className="app-name">Author's Haven</h1>
-    <h2 className="team-name">LIT Kigali</h2>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="app-name">Author's Haven</h1>
+        <h2 className="team-name">LIT Kigali</h2>
+        <Routes />
+      </div>
+    );
+  }
+}
+// const App = () => (
+
+// );
 
 export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const HomeComponent = () => {
+  return (
+    <h1 style={{ background: "blue", color: "white" }}>Login Component</h1>
+  );
+};
+
+export default HomeComponent;
