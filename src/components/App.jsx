5a11c68a3e9cc108ddaadb6fa55d3da126144488
nkfab <fabjeezy@gmail.com>
2019-04-01 11:37:22 +0200

    amend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,19 @@ import './App.scss';
 import Routes from '../routes';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      appName: "Author's haven",
+      teamName: 'Lit Kigali'
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1 className="app-name">Author's Haven</h1>
-        <h2 className="team-name">LIT Kigali</h2>
+        <h1 className="app-name">{this.state.appName}</h1>
+        <h2 className="team-name">{this.state.teamName}</h2>
         <Routes />
       </div>
     );
