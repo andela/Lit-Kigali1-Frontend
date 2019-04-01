9ac6155faa62b65befa0bb1abae346a7fd271787
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-04-01 11:08:06 +0200

    chore: add --coverage argument to jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$'
-}
+  testEnvironment: "node",
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.js"],
+  coveragePathIgnorePatterns: ["coverage", "node_modules"]
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
+<<<<<<< HEAD
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 module.hot.accept();
+=======
+import App from "./components/App.jsx";
+
+>>>>>>> chore: add --coverage argument to jest tests
 render(
   <Provider store={store}>
     <App />
