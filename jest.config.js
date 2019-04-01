e285b0ed84f4e6ed5311621eaad4a02e401a3222
Nziranziza <nziranzizadaniel@gmail.com>
2019-04-01 09:22:14 +0200

    feat(travis) update travisCI - add new lines

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,4 @@ module.exports = {
     testEnvironment: 'node',
     moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  };
+  }
