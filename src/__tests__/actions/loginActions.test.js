a4b6d105e9d9e2f6191e0d0e379ae827230b10dc
filschristian <filschristian94@gmail.com>
2019-04-10 10:36:30 +0200

    feat(login): add tests

--- src/__tests__/actions/loginActions.test.js
+++ src/__tests__/actions/loginActions.test.js
@@ -1,5 +1,19 @@
-import { inputHandler } from '../../redux/actions/loginActions';
-import { HANDLE_LOGIN_INPUT } from '../../redux/actions-types/loginTypes';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {
+  inputHandler,
+  validationResponse,
+  validateCredentials,
+} from '../../redux/actions/loginActions';
+import {
+  HANDLE_LOGIN_INPUT,
+  INPUT_VALIDATION_FAILURE,
+  INPUT_VALIDATION_SUCCESS,
+} from '../../redux/actions-types/loginTypes';
+
+const mockStore = configureStore([thunk]);
+let store;
+jest.setTimeout(30000);
 
 describe('Login Actions', () => {
   test('should return filed and its value', () => {
@@ -11,5 +25,97 @@ describe('Login Actions', () => {
       },
     };
     expect(inputHandler({ field: 'username', value: 'John' })).toEqual(expectedAction);
+  });
+
+  test('Should return validation response', () => {
+    const expectedAction = {
+      type: INPUT_VALIDATION_FAILURE,
+      payload: 'Password must be greater than 6 characters',
+    };
+    expect(validationResponse('Password must be greater than 6 characters')).toEqual(
+      expectedAction,
+    );
+  });
+
+  describe('asynchronous actions', () => {
+    beforeEach(() => {
+      store = mockStore({});
+    });
+
+    test('Validation - should return validation response', () => {
+      expect.assertions(1);
+      const expectedAction = [
+        {
+          type: INPUT_VALIDATION_FAILURE,
+          payload: {
+            response: {
+              message: 'Password and username must be at least 6 characters',
+            },
+          },
+        },
+      ];
+      return store
+        .dispatch(validateCredentials({ username: 'chris', password: 'chris' }))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedAction);
+        });
+    });
+
+    test('Validation - should return username validation response', () => {
+      expect.assertions(1);
+      const expectedAction = [
+        {
+          type: INPUT_VALIDATION_FAILURE,
+          payload: {
+            response: {
+              message: 'Username must be at least 6 characters',
+            },
+          },
+        },
+      ];
+      return store
+        .dispatch(validateCredentials({ username: 'chris', password: 'christian' }))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedAction);
+        });
+    });
+
+    test('Validation - should return password validation response', () => {
+      expect.assertions(1);
+      const expectedAction = [
+        {
+          type: INPUT_VALIDATION_FAILURE,
+          payload: {
+            response: {
+              message: 'Password must be at least 6 characters',
+            },
+          },
+        },
+      ];
+      return store
+        .dispatch(validateCredentials({ username: 'christian', password: 'chris' }))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedAction);
+        });
+    });
+
+    test('Validation - should return validation response', () => {
+      expect.assertions(1);
+      const expectedAction = [
+        {
+          type: INPUT_VALIDATION_SUCCESS,
+          payload: {
+            response: {
+              message: 'Ok',
+            },
+          },
+        },
+      ];
+      return store
+        .dispatch(validateCredentials({ username: 'christian', password: 'christian' }))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedAction);
+        });
+    });
   });
 });
