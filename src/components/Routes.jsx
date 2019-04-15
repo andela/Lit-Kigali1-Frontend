e75f31063417c1e0f5ff4f2d00f363cd4704ef54
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-04-15 15:32:19 +0200

    feat: fix signup tests

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -6,12 +6,9 @@ import Login from './Auth';
 import ResetPassword from './ForgotPassword/ResetPassword';
 import ForgotPassword from './ForgotPassword/ForgotPassword';
 import ForgotPasswordMessage from './ForgotPassword/ForgotPasswordMessage';
-<<<<<<< HEAD
 import Auth from './Auth';
 import ConfirmedEmailMessage from './Auth/ConfirmedEmailMessage';
-=======
 import ProfileView from './Profile/ProfileView';
->>>>>>> feat: implement follow a user feature
 
 const Routes = () => (
   <div>
@@ -20,15 +17,12 @@ const Routes = () => (
     <Route exact path="/auth" component={Auth} />
     <Route exact path="/forgot-password-message" component={ForgotPasswordMessage} />
     <Route exact path="/users/:userId/reset/:resetCode" component={ResetPassword} />
-<<<<<<< HEAD
     <Route
       exact
       path="/users/:userId/confirm_email/:confirmationCode"
       component={ConfirmedEmailMessage}
     />
-=======
     <Route exact path="/profiles/:username" component={ProfileView} />
->>>>>>> feat: implement follow a user feature
   </div>
 );
 
