a9f501d34c817d8eff130b430a54fce7d345992f
caleb <mcaleb808@gmail.com>
2019-04-24 10:41:45 +0200

    feat(pagination): adds pagination for articles

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -4,34 +4,83 @@ import { PropTypes } from 'prop-types';
 export class Pagination extends Component {
   navigateTo = (e) => {
     const { history, url } = this.props;
-    console.log(e.target.dataset);
     if (history && url) {
       history.push(`${url}?page=${e.target.dataset.value}`);
       window.location.reload();
     }
   };
 
+  onNext = () => {
+    const { currentPage, totalPages } = this.props;
+    if (currentPage < totalPages) {
+      const next = (
+        <i
+          className="fa fa-angle-right"
+          data-value={currentPage + 1}
+          key={currentPage + 1}
+          onClick={this.navigateTo}
+          alt="Next"
+        />
+      );
+      return next;
+    }
+  };
+
+  onPrev = () => {
+    const { currentPage } = this.props;
+    if (currentPage > 1) {
+      const next = (
+        <i
+          className="fa fa-angle-left"
+          data-value={currentPage - 1}
+          key={currentPage - 1}
+          onClick={this.navigateTo}
+          alt="Previous"
+        />
+      );
+      return next;
+    }
+  };
+
   PaginationIcon = () => {
     const { totalPages, currentPage } = this.props;
-    const icon = [];
-    if (totalPages <= 5) {
-    }
+    const parent = [];
+    let iconNumber = 1;
+
     for (let i = 1; i <= totalPages; i++) {
-      if (i === currentPage) {
-        icon.push(
-          <li data-value={i} className="page current" onClick={this.navigateTo}>
-            {i}
+      if (iconNumber === currentPage) {
+        parent.push(
+          <li
+            data-value={iconNumber}
+            key={iconNumber}
+            className="page current"
+            onClick={this.navigateTo}
+          >
+            {iconNumber}
           </li>,
         );
       } else {
-        icon.push(
-          <li data-value={i} className="page" onClick={this.navigateTo}>
-            {i}
+        parent.push(
+          <li data-value={iconNumber} key={iconNumber} className="page" onClick={this.navigateTo}>
+            {iconNumber}
           </li>,
         );
       }
+      iconNumber++;
     }
-    return icon;
+    if (currentPage === 1) {
+      return parent.slice(currentPage - 1, currentPage + 4);
+    }
+    if (currentPage === 2) {
+      return parent.slice(currentPage - 2, currentPage + 3);
+    }
+    if (currentPage === totalPages) {
+      return parent.slice(currentPage - 5, currentPage);
+    }
+    if (currentPage === totalPages - 1) {
+      return parent.slice(currentPage - 4, currentPage + 1);
+    }
+    return parent.slice(currentPage - 3, currentPage + 2);
   };
 
   render() {
@@ -41,13 +90,9 @@ export class Pagination extends Component {
         <span data-value={1} className="page" onClick={this.navigateTo}>
           First
         </span>
-        <span>
-          <i className="fa fa-angle-left" />
-        </span>
+        <span>{this.onPrev()}</span>
         <ul className="pages">{this.PaginationIcon()}</ul>
-        <span>
-          <i className="fa fa-angle-right" />
-        </span>
+        <span>{this.onNext()}</span>
         <span data-value={totalPages} className="page" onClick={this.navigateTo}>
           Last
         </span>
