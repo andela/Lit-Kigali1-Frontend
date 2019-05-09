ace4bb5a1f5d6888964ebc8795c8fb0569d779e3
filschristian <filschristian94@gmail.com>
2019-05-09 11:27:42 +0200

    feat(like/dislike comment): rebase and fix conflicts

--- src/components/Comment/CommentRender.jsx
+++ src/components/Comment/CommentRender.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 import Textarea from 'react-textarea-autosize';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import Button from '../common/Button/Button';
@@ -24,7 +23,58 @@ export class CommentRender extends React.Component {
     fetchDislikes(articleSlug, id);
   }
 
-  commentBox = () => {
+  commentForm = (id) => {
+    const { updateComment, enterPress, updateBody } = this.props;
+    return (
+      <form>
+        <Textarea
+          className="comment-textarea"
+          placeholder="Update your comment..."
+          type="text"
+          value={updateBody}
+          onKeyDown={(e) => {
+            enterPress(e, updateComment, id);
+            this.closeCommentInput(e);
+          }}
+          onBlur={this.onFocusOut}
+          onChange={this.onChange}
+          autoFocus
+        />
+      </form>
+    );
+  };
+
+  onEditComment = (body) => {
+    const { inputHandler } = this.props;
+    this.setState({
+      isEdit: true,
+    });
+    inputHandler(body);
+  };
+
+  onFocusOut = () => {
+    this.setState({
+      isEdit: false,
+    });
+  };
+
+  closeCommentInput = (e) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      if (!e.target.value.trim()) {
+        e.preventDefault();
+        return;
+      }
+      this.onFocusOut();
+      e.preventDefault();
+    }
+  };
+
+  onChange = (e) => {
+    const { inputHandler } = this.props;
+    inputHandler(e.target.value);
+  };
+
+  render() {
     const { isEdit } = this.state;
     const {
       comment,
@@ -37,14 +87,14 @@ export class CommentRender extends React.Component {
     return (
       <div key={comment.id} className="comment-box">
         <div className="comment-header">
-          <Link className="author-name" to={`../profiles/${comment.author.username}`}>
+          <a className="author-name" to={`../profiles/${comment.author.username}`}>
             <img
               src={comment.author.image ? comment.author.image : avatar}
               alt=""
               className="profile-avatar"
             />
             {comment.author.username}
-          </Link>
+          </a>
           {comment.userId === currentUser.id && (
             <span className="control-btn">
               <Button
@@ -123,107 +173,6 @@ export class CommentRender extends React.Component {
             </div>
           </span>
         </div>
-      </div>
-    );
-  };
-
-  commentForm = (id) => {
-    const { updateComment, enterPress, updateBody } = this.props;
-    return (
-      <form>
-        <Textarea
-          className="comment-textarea"
-          placeholder="Update your comment..."
-          type="text"
-          value={updateBody}
-          onKeyDown={(e) => {
-            enterPress(e, updateComment, id);
-            this.closeCommentInput(e);
-          }}
-          onBlur={this.onFocusOut}
-          onChange={this.onChange}
-          autoFocus
-        />
-      </form>
-    );
-  };
-
-  onEditComment = (body) => {
-    const { inputHandler } = this.props;
-    this.setState({
-      isEdit: true,
-    });
-    inputHandler(body);
-  };
-
-  onFocusOut = () => {
-    this.setState({
-      isEdit: false,
-    });
-  };
-
-  closeCommentInput = (e) => {
-    if (e.keyCode === 13 && e.shiftKey === false) {
-      if (!e.target.value.trim()) {
-        e.preventDefault();
-        return;
-      }
-      this.onFocusOut();
-      e.preventDefault();
-    }
-  };
-
-  onChange = (e) => {
-    const { inputHandler } = this.props;
-    inputHandler(e.target.value);
-  };
-
-  render() {
-    const { isEdit } = this.state;
-    const {
-      comment, currentUser, onDeleteComment, articleSlug,
-    } = this.props;
-    return (
-      <div key={comment.id} className="comment-box">
-        <div className="comment-header">
-          <a className="author-name" href={`../profiles/${comment.author.username}`}>
-            <img
-              src={comment.author.image ? comment.author.image : avatar}
-              alt=""
-              className="profile-avatar"
-            />
-            {comment.author.username}
-          </a>
-          {comment.userId === currentUser.id && (
-            <span className="control-btn">
-              <Button
-                classes="my-article-delete"
-                data-el="delete-btn"
-                onClick={() => onDeleteComment(comment.id, articleSlug)}
-              >
-                <i className="fa fa-trash" />
-              </Button>
-              <Button
-                data-el="edit-btn"
-                classes="my-comment-update"
-                onClick={() => this.onEditComment(comment.body)}
-              >
-                <i className="fa fa-edit" />
-              </Button>
-            </span>
-          )}
-        </div>
-        <div
-          onDoubleClick={() => comment.userId === currentUser.id && this.onEditComment(comment.body)
-          }
-          data-el="comment-container"
-        >
-          {isEdit ? this.commentForm(comment.id) : comment.body}
-          <span className="comment-time">
-            {comment.version === 'edited' && ` (${comment.version})`}
-          </span>
-        </div>
-        <div className="comment-time">{moment(comment.createdAt).fromNow()}</div>
       </div>
     );
   }
